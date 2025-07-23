b796f08ffd11a98771f19774fe7d641914012045
Danilo <dan.trabalho94@gmail.com>
2025-07-23 04:11:57 -0300

    Subindo melhoria logo

--- src/layouts/Auth/index.vue
+++ src/layouts/Auth/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="min-h-screen flex flex-col bg-cineflix-black">
         <header class="bg-cineflix-black p-4 flex justify-center items-center">
-            <img src="../../assets/img/CineFlix.png" width="150" alt="Logo CineFlix">
+            <img src="../../assets/img/CineFlix.webp" width="150" alt="Logo CineFlix">
         </header>
 
         <main class="flex-grow flex justify-center pt-8 pb-8">
