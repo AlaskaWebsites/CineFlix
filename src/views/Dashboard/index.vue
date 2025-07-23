8936c0d2a5cd92e42ea27b484c515fb494a8bc76
Danilo <dan.trabalho94@gmail.com>
2025-07-23 04:13:39 -0300

    Subindo ajuste logo dashboard

--- src/views/Dashboard/index.vue
+++ src/views/Dashboard/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="bg-cineflix-black text-cineflix-white p-4">
-            <img src="../../assets/img/CineFlix.png" width="200" alt="Logo CineFlix">
+            <img src="../../assets/img/CineFlix.webp" width="200" alt="Logo CineFlix">
             <h1 class="text-3xl font-bold text-cineflix-red">Bem-vindo a Dashboard CineFlix!</h1>
             <p class="text-cineflix-dark-gray">Gerencie seus filmes favoritos.</p>
             <button class="bg-cineflix-red text-cineflix-white p-2 rounded">Entrar</button>
