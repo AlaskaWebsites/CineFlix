5faaca8872cf4cd9f5cf6efa967e378cb72cc216
Danilo <dan.trabalho94@gmail.com>
2025-07-24 06:51:56 -0300

    Subindo ajuste mobile auth

--- src/layouts/Auth/index.vue
+++ src/layouts/Auth/index.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="min-h-screen flex flex-col bg-cineflix-black">
-        <header class="bg-cineflix-black p-4 flex justify-center items-center">
-            <img src="../../assets/img/CineFlix.webp" width="150" alt="Logo CineFlix">
+        <header>
+            <img src="../../assets/img/CineFlix.webp" width="200" alt="CineFlix Logo" class="mx-auto">
         </header>
 
-        <main class="flex-grow flex justify-center pt-8 pb-8">
-            <slot></slot>
+        <main class="flex-grow flex justify-center py-8 px-4 sm:px-6 md:px-8">
+            <router-view />
         </main>
+
+        <footer class="text-center text-gray-400 text-sm py-4">
+            &copy; {{ new Date().getFullYear() }} CineFlix. Todos os direitos reservados.
+        </footer>
     </div>
 </template>
 
+<script setup lang="ts">
+// Nenhuma alteração aqui
+</script>
+
 <style scoped>
-/* Sem alterações */
+/* Nenhuma alteração aqui */
 </style>
